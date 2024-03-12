7da45e05505ef73b588914df96e91b0a44feb514
Rowin Schmidt <rowinschmidt@gmail.com>
2024-03-12 16:27:25 +0100

    Support pagina bereikbaar gemaakt met redirect

---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { onMount } from 'svelte';
+    
+    onMount(() => {
+        if (typeof window !== 'undefined') {
+            window.location.replace("https://futurereadydesign.nl/support");
+        }
+    });
+</script>
