3584996b3a1af94924d8072baf84e2bb34ad1017
Amberhva <support@futureready.design>
2024-04-15 12:29:41 +0200

    Eerste versies van animaties
    Eerste versies van animaties

--- src/components/TeamCursor.svelte
+++ src/components/TeamCursor.svelte
@@ -1,9 +1,47 @@
-<section>
+<script>
+    let isMouseInside = false;
+
+    function handleMouseEnter() {
+        const section = document.getElementById('cursor-effect');
+        const svg = document.getElementById('you');
+        
+        // Verander de cursor in de SVG
+        section.style.cursor = `url('data:image/svg+xml;utf8,${encodeURIComponent(svg.outerHTML)}'), auto`;
+        // Stop de animatie
+        svg.style.animationPlayState = 'paused';
+        isMouseInside = true;
+    }
+
+    function handleMouseLeave() {
+        const section = document.getElementById('cursor-effect');
+        const svg = document.getElementById('you');
+        
+        // Verander de cursor terug naar de standaard
+        section.style.cursor = 'auto';
+        // Start de animatie als de muis buiten de sectie is
+        if (!isMouseInside) {
+            svg.style.animationPlayState = 'running';
+        }
+    }
+
+    function handleSvgMouseEnter() {
+        isMouseInside = true;
+    }
+
+    function handleSvgMouseLeave() {
+        const svg = document.getElementById('you');
+        isMouseInside = false;
+        // Start de animatie als de muis buiten de SVG is
+        svg.style.animationPlayState = 'running';
+    }
+</script>
+
+<section id="cursor-effect"on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave}>
     <h2>we are driven by the goal of empowering individuals and organisations with human-first design.</h2>
     <div></div>
 
     <!-- YOU -->
-    <svg class="you" width="379" height="337" viewBox="0 0 379 337" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg id="you" width="379" height="337" viewBox="0 0 379 337" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g filter="url(#filter0_dd_2369_71)">
         <rect x="150" y="150.333" width="79" height="36" rx="7.5" fill="url(#paint0_linear_2369_71)"/>
         <path d="M175.31 170.293L170.396 162.733H172.79L176.624 168.763H175.508L179.342 162.733H181.7L176.75 170.293H175.31ZM175.022 175.333V168.997H177.092V175.333H175.022ZM184.515 175.513C183.675 175.513 182.913 175.315 182.229 174.919C181.545 174.511 180.999 173.965 180.591 173.281C180.195 172.597 179.997 171.835 179.997 170.995C179.997 170.155 180.195 169.399 180.591 168.727C180.999 168.055 181.545 167.521 182.229 167.125C182.913 166.717 183.675 166.513 184.515 166.513C185.367 166.513 186.135 166.711 186.819 167.107C187.503 167.503 188.043 168.043 188.439 168.727C188.847 169.399 189.051 170.155 189.051 170.995C189.051 171.835 188.847 172.597 188.439 173.281C188.043 173.965 187.503 174.511 186.819 174.919C186.135 175.315 185.367 175.513 184.515 175.513ZM184.515 173.605C185.007 173.605 185.439 173.497 185.811 173.281C186.195 173.053 186.489 172.741 186.693 172.345C186.909 171.949 187.017 171.499 187.017 170.995C187.017 170.491 186.909 170.047 186.693 169.663C186.477 169.279 186.183 168.979 185.811 168.763C185.439 168.535 185.007 168.421 184.515 168.421C184.035 168.421 183.603 168.535 183.219 168.763C182.847 168.979 182.553 169.279 182.337 169.663C182.133 170.047 182.031 170.491 182.031 170.995C182.031 171.499 182.133 171.949 182.337 172.345C182.553 172.741 182.847 173.053 183.219 173.281C183.603 173.497 184.035 173.605 184.515 173.605ZM194.244 175.513C193.512 175.513 192.858 175.351 192.282 175.027C191.718 174.703 191.274 174.259 190.95 173.695C190.638 173.119 190.482 172.459 190.482 171.715V166.693H192.462V171.625C192.462 172.045 192.528 172.405 192.66 172.705C192.804 172.993 193.008 173.215 193.272 173.371C193.548 173.527 193.872 173.605 194.244 173.605C194.82 173.605 195.258 173.437 195.558 173.101C195.87 172.753 196.026 172.261 196.026 171.625V166.693H198.006V171.715C198.006 172.471 197.844 173.137 197.52 173.713C197.208 174.277 196.77 174.721 196.206 175.045C195.642 175.357 194.988 175.513 194.244 175.513ZM202.75 171.157V167.683H203.236C203.632 167.683 203.974 167.611 204.262 167.467C204.562 167.311 204.79 167.101 204.946 166.837C205.114 166.561 205.198 166.243 205.198 165.883C205.198 165.523 205.12 165.211 204.964 164.947C204.808 164.683 204.586 164.479 204.298 164.335C204.01 164.191 203.668 164.119 203.272 164.119C202.756 164.119 202.294 164.263 201.886 164.551C201.49 164.827 201.172 165.241 200.932 165.793L199.492 164.605C199.852 163.825 200.368 163.231 201.04 162.823C201.724 162.403 202.504 162.193 203.38 162.193C204.136 162.193 204.802 162.343 205.378 162.643C205.966 162.943 206.422 163.363 206.746 163.903C207.07 164.431 207.232 165.043 207.232 165.739C207.232 166.387 207.088 166.963 206.8 167.467C206.524 167.971 206.128 168.391 205.612 168.727C205.096 169.063 204.484 169.297 203.776 169.429L204.64 168.547L204.46 171.157H202.75ZM203.596 175.513C203.212 175.513 202.894 175.387 202.642 175.135C202.402 174.871 202.282 174.553 202.282 174.181C202.282 173.797 202.402 173.479 202.642 173.227C202.894 172.975 203.212 172.849 203.596 172.849C203.98 172.849 204.292 172.975 204.532 173.227C204.784 173.479 204.91 173.797 204.91 174.181C204.91 174.553 204.784 174.871 204.532 175.135C204.292 175.387 203.98 175.513 203.596 175.513Z" fill="black"/>
@@ -355,6 +393,10 @@
 </section>
 
 <style lang="scss">
+    div {
+        margin-top: 10em;
+    }
+
     section {
         margin-top: 13em;
 
@@ -380,60 +422,174 @@
             border-radius: 100%;
         }
 
-        .you {
+        #you {
             display: block;
             margin: -25em auto; 
-            animation: floatAnimation 3s ease-in-out infinite alternate;
+            animation: YouAnimation 8s ease-in-out infinite alternate;
         }
         .douwe {
             position: absolute;
             left: 22%; 
             top: 25%; 
             transform: translateY(-50%); 
+            animation: DouweAnimation 6s ease-in-out infinite alternate;
         }
         .mark {
             position: absolute;
             right: 10%; 
             top: 50%; 
             transform: translateY(-50%); 
+            animation: MarkAnimation 9s ease-in-out infinite alternate;
         }
         .robbin {
             position: absolute;
             left: 15%; 
             bottom: 25%; 
+            animation: RobbinAnimation 7s ease-in-out infinite alternate;
         }
         .rowin {
             position: absolute;
             right: 18%;
             top: 10%;
+            animation: RowinAnimation 5s ease-in-out infinite alternate;
         }
         .tyco {
             position: absolute;
             left: 35%;
             bottom: 10%;
+            animation: TycoAnimation 10s ease-in-out infinite alternate;
         }
         .yunus {
             position: absolute;
             right: 22%;
             bottom: 15%;
+            animation: YunusAnimation 7s ease-in-out infinite alternate;
         }
     }
 
-    @keyframes floatAnimation {
+    @keyframes YouAnimation {
         0% {
-        transform: translate(0, 0); // Beginpositie
+            transform: translate(0, 0); 
         }
         25% {
-            transform: translate(5em, 5em); // Schuin naar rechts onder
+            transform: translate(5em, 5em); 
         }
         50% {
-            transform: translate(0, 5em); // Schuin naar beneden
+            transform: translate(0, 5em); 
         }
         75% {
-            transform: translate(-50px, 50px); // Schuin naar links onder
+            transform: translate(-50px, 50px);
         }
         100% {
-            transform: translate(0, 0); // Terug naar beginpositie
+            transform: translate(0, 0); 
+        }
+    }
+
+    @keyframes DouweAnimation {
+        0% {
+            transform: translate(0, 0);
+        }
+        25% {
+            transform: translate(30px, 70px);
+        }
+        50% {
+            transform: translate(-45px, 35px);
+        }
+        75% {
+            transform: translate(60px, -12.5px);
+        }
+        100% {
+            transform: translate(0, 0);
+        }
+    }
+
+    @keyframes MarkAnimation {
+        0% {
+            transform: translateX(0);
+        }
+        25% {
+            transform: translateX(50px);
+        }
+        50% {
+            transform: translateX(100px);
+        }
+        75% {
+            transform: translateX(120px);
+        }
+        100% {
+            transform: translateX(130px);
+        }
+    }
+
+    @keyframes RobbinAnimation {
+        0% {
+            transform: translateY(0) translateX(0);
+        }
+        25% {
+            transform: translateY(-50px) translateX(50px);
+        }
+        50% {
+            transform: translateY(0) translateX(100px);
+        }
+        75% {
+            transform: translateY(50px) translateX(50px);
+        }
+        100% {
+            transform: translateY(0) translateX(0);
+        }
+    }
+
+    @keyframes RowinAnimation {
+        0% {
+            transform: translateY(0);
+        }
+        25% {
+            transform: translateY(-50px);
+        }
+        50% {
+            transform: translateY(0);
+        }
+        75% {
+            transform: translateY(-50px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes TycoAnimation {
+        0% {
+            transform: translate(0, 0);
+        }
+        25% {
+            transform: translate(-50px, -50px);
+        }
+        50% {
+            transform: translate(-100px, -100px);
+        }
+        75% {
+            transform: translate(-150px, -150px);
+        }
+        100% {
+            transform: translate(-200px, -200px);
+        }
+    }
+
+    @keyframes YunusAnimation {
+        0% {
+            transform: translateY(0);
+        }
+        25% {
+            transform: translateY(-25px);
+        }
+        50% {
+            transform: translateY(0);
+        }
+        75% {
+            transform: translateY(25px);
+        }
+        100% {
+            transform: translateY(0);
         }
     }
 </style>
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -4,7 +4,6 @@
 
     // Importeer het juiste component. In dit geval TeamCursor.svelte
     import TeamCursor from '/src/components/TeamCursor.svelte'
-
 </script>
 
 <!-- Laat het juiste component zien op de pagina -->
