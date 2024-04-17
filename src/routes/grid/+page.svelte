9b5ff1e8919fb0df8fce98e2fcc4cb87ce3a4fc4
Tycovds <tycovds@gmail.com>
2024-04-17 15:16:35 +0200

    responsive grid

--- src/routes/grid/+page.svelte
+++ src/routes/grid/+page.svelte
@@ -28,6 +28,16 @@
       <GridItem imgPath={imagePath}></GridItem>
     {/each}
   </div>
+  <div class="google-review">
+    <div class="top">
+      <div class="google-logo">
+        <img src="/assets/google-blur.svg" alt="blurred google logo" />
+        <img src="/assets/google-logo.svg" alt="google logo" />
+      </div>
+      <img src="/assets/stars.svg" alt="review stars" />
+    </div>
+    <p>5 stars on Google!</p>
+  </div>
 </section>
 
 <style lang="scss">
@@ -36,13 +46,13 @@
   section {
     text-align: center;
     h2 {
-        font-size: 50px;
-        font-weight: 700;
+      font-size: 50px;
+      font-weight: 700;
     }
     p {
-        opacity: 0.5;
-        font-size: 20px;
-        margin-bottom: 75px;
+      opacity: 0.5;
+      font-size: 20px;
+      margin-bottom: 75px;
     }
   }
   .organisation-grid {
@@ -51,5 +61,45 @@
     grid-template-rows: repeat(4, 1fr);
     grid-column-gap: 20px;
     grid-row-gap: 20px;
+    margin-bottom: 55px;
+  }
+  .google-review {
+    .top {
+      display: flex;
+      align-items: center;
+      gap: 15px;
+      justify-content: center;
+      .google-logo {
+        position: relative;
+        width: 40px;
+        height: 40px;
+        img {
+          position: absolute;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50%, -50%);
+        }
+      }
+    }
+    p {
+      margin-top: 10px;;
+    }
+  }
+
+  @media screen and (max-width: 1024px) {
+    section {
+      p {
+        margin-bottom: 35px;
+      }
+    }
+    .organisation-grid {
+      grid-template-columns: repeat(3, 1fr);
+      margin-bottom: 40px;
+    }
+  }
+  @media screen and (max-width: 676px) {
+    .organisation-grid {
+      grid-template-columns: repeat(2, 1fr);
+    }
   }
 </style>
