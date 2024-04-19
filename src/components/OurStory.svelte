7d37ebdbdc005f1e4578a09923cd66f9d50975b8
Daan Carstens <daanbrother@icloud.com>
2024-04-19 14:21:21 +0200

    responsive fixes story component + pagina voor projects

--- src/components/OurStory.svelte
+++ src/components/OurStory.svelte
@@ -1,51 +1,44 @@
 <div class="story-container">
-    <img class="story-image" src="/assets/content/foto-van-robbin-en-yunus.png" alt="foto van Robbin en Yunus">
-    <div class="content-container">
-      <h2 class="story-title">Our story in brief...</h2>
-      <p class="story-text">“we believe in technology that enriches real-life experiences, acting as an extension rather than a replacement.”</p>
-      <button class="story-button">Learn our story</button>
-    </div>
+  <img src="/assets/content/foto-van-robbin-en-yunus.png" alt="foto van Robbin en Yunus">
+  <div>
+    <h2>Our story in brief...</h2>
+    <p>“we believe in technology that enriches real-life experiences, acting as an extension rather than a replacement.”</p>
+    <button>Learn our story</button>
   </div>
+</div>
 
-  <style lang="scss">
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
 
+<style lang="scss">
 .story-container {
   max-width: 1200px;
   margin: 0 auto;
   padding: 20px;
   display: flex;
   flex-direction: column;
-  align-items: left;
+  align-items: flex-start;
   text-align: left;
-  color: var(white);
+  color: white;
 
-  .story-image {
-    max-width: 100%;
-    width: 30em;
+  img {
+    width: 100%;
     height: auto;
   }
 
-  .story-title {
-    font-size: 20px;
+  p {
+    color: white;
+    margin-top: 10px;
+    font-size: 1.5em;
+    max-width: 100%;
+  }
+
+  h2 {
+    font-size: 1em;
     font-weight: 200;
     margin-top: 20px;
-    opacity: 50%;
-    margin-left: 1em;
+    opacity: 0.5;
   }
 
-  .story-text {
-    margin-top: 10px;
-    font-size: 35px;
-    max-width: 12em;
-    margin-left: .5em;
-  }
-
-  .story-button {
+  button {
     margin-top: 20px;
     padding: 10px 20px;
     background-color: rgba(0, 0, 0, 0.15);
@@ -55,7 +48,6 @@
     font-size: 15px;
     cursor: pointer;
     transition: background-color 0.3s ease;
-    margin-left: 1em;
 
     &:hover {
       background-color: rgba(0, 0, 0, 0.25);
@@ -65,16 +57,19 @@
 
   @media (min-width: 850px) {
     flex-direction: row;
-    text-align: left;
+    align-items: center;
+    justify-content: center;
 
-    .story-image {
-      margin-right: 20px;
+    img {
+      margin-right: 2em;
+      max-width: 35em;
     }
 
-    .story-title,
-    .story-text {
+    p {
       text-align: left;
+      font-size: 1.8em;
     }
   }
 }
+
   </style>
--- src/routes/daanComponents/+page.svelte
+++ src/routes/daanComponents/+page.svelte
@@ -5,9 +5,11 @@
     // Importeer het juiste component. In dit geval TeamCursor.svelte
     import Nav from '/src/components/Nav.svelte'
     import OurStory from '/src/components/OurStory.svelte'
+    import Projects from '/src/components/Projects.svelte'
 
 </script>
 
 <!-- Laat het juiste component zien op de pagina -->
 <Nav/>
 <OurStory/>
+<Projects/>
