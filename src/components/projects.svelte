5ef44237b11b36bf1676caead8e40eaa917d771f
DaanCarst <daanbrother@icloud.com>
2024-04-28 13:48:08 +0200

    responsive projects page

--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -38,7 +38,7 @@
             <button>More about this project →</button>
         </section>
     </article>
-    <button>Explore more of our work →</button>
+    <button>Explore more of our work  →</button>
 </section>
 
 <style lang="scss">
@@ -48,6 +48,7 @@
     justify-content: center;
     align-items: center;
     margin-top: 2em;
+    font-size: 1.2em;
 }
 
 .content-container {
@@ -64,6 +65,84 @@
 .left-container, .right-container {
     display: flex;
     flex-direction: row;
+
+    h4 {
+        opacity: 50%;
+        margin-bottom: .6em;
+    }
+
+    h3 {
+        font-size: 1.8em;
+        margin-bottom: .6em;
+    }
+
+    p {
+        margin-bottom: 1em;
+        opacity: 65%;
+        font-size: 1em;
+    }
+
+    button {
+        margin-bottom: .6em;
+        color: white;
+        background: none;
+        border: none;
+        cursor: pointer;
+        font-size: .8em;
+    }
+}
+
+.left-container:nth-last-child(2) {
+    margin-top: 5em; 
+}
+
+.left-container {
+    display: flex;
+    flex-direction: row;
+
+    .image-container {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start; 
+    }
+
+    .text-container {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start; 
+        padding: 0 0.5em;
+    }
+}
+
+.right-container {
+    display: flex;
+    flex-direction: row-reverse; 
+    margin-top: 5em; 
+
+    h4 {
+        align-self: flex-start;
+    }
+
+    button {
+        align-self: flex-start;
+    }
+
+    .image-container {
+        display: flex;
+        align-items: center;
+        justify-content: flex-end; 
+    }
+
+    .text-container {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-end; 
+        padding: 0 0.5em;
+    }
 }
 
 .left-container {
@@ -81,16 +160,43 @@
 }
 
 .image-container, .text-container {
-    flex: 1;
+    flex: .75;
 }
 
-@media (max-width: 768px) {
+.image-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 1em;
+}
+
+.text-container {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 0 1em;
+}
+
+@media (max-width: 950px) {
     .left-container, .right-container {
         flex-direction: column;
     }
 
-    .image-container, .text-container {
+    .image-container img, .text-container {
         width: 100%;
+    }
+
+    .heading-container {
+        padding: 1em;
+    }
+
+    .content-container {
+        margin: 0;
+    }
+
+    h4 {
+        margin-top: 1em;
     }
 }
 </style>
