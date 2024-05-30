29c386e299422b1e9ece90f1ca5c7856627f323d
Rowin Schmidt <rowinschmidt@gmail.com>
2024-05-30 18:20:52 +0200

    vite fix?

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-// import { sveltekit } from "@sveltejs/kit/vite";
-// import { defineConfig } from "vite";
-// import basicSsl from "@vitejs/plugin-basic-ssl";
-
-// export default defineConfig({
-//     plugins: [sveltekit(), basicSsl()],
-//     server: {
-//         https: true,
-//     },
-// });
-
-import { sveltekit } from '@sveltejs/kit/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl'
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
+    plugins: [sveltekit(), basicSsl()],
     server: {
-        proxy: {}
+        https: true,
     },
-    plugins: [basicSsl(), sveltekit()]
 });
+
+// import { sveltekit } from '@sveltejs/kit/vite';
+// import basicSsl from '@vitejs/plugin-basic-ssl'
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//     server: {
+//         proxy: {}
+//     },
+//     plugins: [basicSsl(), sveltekit()]
+// });
